ea076829e2c0d5a0c478c6f642d28cb16511e519
Michael Bogle <support@mjnshosting.com>
2019-08-31 12:25:47 -0400

    adding count to the -a options

--- password-generator.sh
+++ password-generator.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 #Purpose - Personal Password Generator Script
-#Inputs  - NOthing just options
-#Action  -
-#Status  - Incomplete
+#Inputs  - Length or Count
+#Action  - Generate random passwords 3 different ways
+#Status  - Complete
 
 dictionary=words_alpha-5chars.txt
 dictionary_max=$(wc -l $dictionary| cut -d ' ' -f 1)
@@ -24,13 +24,13 @@ function random_special_char {
 #Generates a random alpha numeric string WITH OUT special characters max characters long
 function genrandom_alphanumeric {
 	max=$1
-	date +%s | sha256sum | base64 | head -c $max
+	echo $RANDOM | sha256sum | base64 | head -c $max
 }
 
 #Generates a random alpha numeric string WITH special characters max characters long
 function genrandom_alphanumeric_special {
 	max=$1
-	date +%s | sha256sum | base64 | head -c $max ;
+	echo $RANDOM | sha256sum | base64 | head -c $max
 }
 
 function choose_word {
@@ -57,34 +57,88 @@ function do_help {
         echo -e "  -w <count>: Count is the amount of the randomly generated passwords to output. \n"
 }
 
+function do_options {
+case $secondary in
+	-c )
+		count=$1
+		i=0
+		if [ -z "$count" ]
+			then
+				echo -e "\nPassword: $(choose_word) \n"
+			else
+				echo ""
+				while [ $i -le $(( $count - 1)) ]
+				do
+					choose_word
+					i=$(( $i+1 ))
+				done
+				echo ""
+		fi
+esac
+}
+
 primary=$1; shift
 case $primary in
 	-h )
 		do_help
 		exit 0
 		;;
+
 	#Generate random alphanumeric
 	-a )
-		password_length=$1
-		if [ -z "$password_length" ]
-			then
-				echo -e "\nPassword:" $(genrandom_alphanumeric 20) "\n"
-			else
-				echo -e "\nPassword:" $(genrandom_alphanumeric $password_length) "\n"
+		password_length=$1; shift
+		secondary=$1; shift
+		count=$1
 
-		fi
+		case $secondary in
+			-c )
+				if [ -z "$count" ]
+					then
+						count=10
+				fi
+
+				i=0
+				if [ -z "$password_length" ]
+					then
+						echo ""
+						while [ $i -le $(( $count - 1)) ]
+						do
+							echo $(genrandom_alphanumeric 20)
+							i=$(( $i+1 ))
+						done
+						echo ""
+					else
+						echo ""
+						while [ $i -le $(( $count - 1)) ]
+						do
+							echo $(genrandom_alphanumeric $password_length)
+							i=$(( $i+1 ))
+						done
+						echo ""
+				fi
+				;;
+			* )
+				if [ -z "$password_length" ]
+					then
+						password=$(genrandom_alphanumeric 20)
+						echo -e "\nPassword: $password \n"
+					else
+						password=$(genrandom_alphanumeric $password_length)
+						echo -e "\nPassword: $password \n"
+				fi
+				;;
+		esac
 		;;
-
 	#Generate random alphanumeric with special character randomly inserted
 	-s )
 		password_length=$1
 		if [ -z "$password_length" ]
 			then
 				password=$((genrandom_alphanumeric_special 20) | sed "s/./&$(random_special_char)/$(genrandom_number 20)" | sed "s/./&$(random_special_char)/$(genrandom_number 20)")
-				echo -e "\nPassword: $password"
+				echo -e "\nPassword: $password \n"
 			else
 				password=$((genrandom_alphanumeric_special $password_length) | sed "s/./&$(random_special_char)/$(genrandom_number $password_length)" | sed "s/./&$(random_special_char)/$(genrandom_number $password_length)")
-				echo -e "\nPassword: $password"
+				echo -e "\nPassword: $password \n"
 
 		fi
 		;;
@@ -96,13 +150,15 @@ case $primary in
 		i=0
 		if [ -z "$count" ]
 			then
-				choose_word
+				echo -e "\nPassword: $(choose_word) \n"
 			else
+				echo ""
 				while [ $i -le $(( $count - 1)) ]
 				do
 					choose_word
 					i=$(( $i+1 ))
 				done
+				echo ""
 		fi
 		;;
 
