5d7277a58baae7970c47ac3fbb8d97aa84c44bb1
Michael Bogle <support@mjnshosting.com>
2019-08-31 21:52:51 -0400

    add checks for dictionary file

--- password-generator.sh
+++ password-generator.sh
@@ -6,7 +6,11 @@
 #Status  - Complete
 
 dictionary=words_alpha-5chars.txt
-dictionary_max=$(wc -l $dictionary| cut -d ' ' -f 1)
+
+if [ -e $dictionary ]
+	then
+		dictionary_max=$(wc -l $dictionary| cut -d ' ' -f 1)
+fi
 
 #Generates a random number between one and the max + 1
 function genrandom_number {
@@ -34,16 +38,21 @@ function genrandom_alphanumeric_special {
 }
 
 function choose_word {
-	max=$dictionary_max
-	line1=$(shuf -i 1-$max -n 1)
-	line2=$(shuf -i 1-$max -n 1)
-	word1=$(sed "${line1}q;d" $dictionary)
-	word1_length=${#word1}
-	word1_case=$(echo $word1 | sed "s/./\U&/$(genrandom_number $word1_length)" | sed "s/./&$(random_special_char)/$(genrandom_number $word1_length)")
-	word2=$(sed "${line2}q;d" $dictionary)
-	word2_length=${#word2}
-	word2_case=$(echo $word2 | sed "s/./\U&/$(genrandom_number $word2_length)" | sed "s/./&$(random_special_char)/$(genrandom_number $word2_length)")
-	echo $word1_case$(genrandom_number 100)$word2_case
+	if [ -e  $dictionary ]
+		then
+			max=$dictionary_max
+			line1=$(shuf -i 1-$max -n 1)
+			line2=$(shuf -i 1-$max -n 1)
+			word1=$(sed "${line1}q;d" $dictionary)
+			word1_length=${#word1}
+			word1_case=$(echo $word1 | sed "s/./\U&/$(genrandom_number $word1_length)" | sed "s/./&$(random_special_char)/$(genrandom_number $word1_length)")
+			word2=$(sed "${line2}q;d" $dictionary)
+			word2_length=${#word2}
+			word2_case=$(echo $word2 | sed "s/./\U&/$(genrandom_number $word2_length)" | sed "s/./&$(random_special_char)/$(genrandom_number $word2_length)")
+			echo $word1_case$(genrandom_number 100)$word2_case
+		else
+			echo -e "\nDictionary file does not exist.\nIs it in the current directory?\nIs the name of the dictionary file $dictionary?\n"
+	fi
 }
 
 function do_help {
@@ -55,26 +64,10 @@ function do_help {
         echo -e "  -a <length>: Length is the length of the randomly generated alphanumeric password. \n      if there is no number after -r it defaults to 20."
         echo -e "  -s <length>: Length is the length of the randomly generated alphanumeric password \n      with special characters randomly inserted. \n      if there is no number after -r it defaults to 20."
         echo -e "  -w <count>: Count is the amount of the randomly generated passwords to output. \n"
-}
+        echo -e "  Secondary Options:"
+	echo -e "  * Valid for -a and -s options. This is not necessary for the -w option *"
+	echo -e "  -c <count>: Count is the amount of the randomly generated passwords to output. \n"
 
-function do_options {
-case $secondary in
-	-c )
-		count=$1
-		i=0
-		if [ -z "$count" ]
-			then
-				echo -e "\nPassword: $(choose_word) \n"
-			else
-				echo ""
-				while [ $i -le $(( $count - 1)) ]
-				do
-					choose_word
-					i=$(( $i+1 ))
-				done
-				echo ""
-		fi
-esac
 }
 
 primary=$1; shift
@@ -100,21 +93,17 @@ case $primary in
 				i=0
 				if [ -z "$password_length" ]
 					then
-						echo ""
 						while [ $i -le $(( $count - 1)) ]
 						do
 							echo $(genrandom_alphanumeric 20)
 							i=$(( $i+1 ))
 						done
-						echo ""
 					else
-						echo ""
 						while [ $i -le $(( $count - 1)) ]
 						do
 							echo $(genrandom_alphanumeric $password_length)
 							i=$(( $i+1 ))
 						done
-						echo ""
 				fi
 				;;
 			* )
@@ -131,34 +120,71 @@ case $primary in
 		;;
 	#Generate random alphanumeric with special character randomly inserted
 	-s )
-		password_length=$1
-		if [ -z "$password_length" ]
-			then
-				password=$((genrandom_alphanumeric_special 20) | sed "s/./&$(random_special_char)/$(genrandom_number 20)" | sed "s/./&$(random_special_char)/$(genrandom_number 20)")
-				echo -e "\nPassword: $password \n"
-			else
-				password=$((genrandom_alphanumeric_special $password_length) | sed "s/./&$(random_special_char)/$(genrandom_number $password_length)" | sed "s/./&$(random_special_char)/$(genrandom_number $password_length)")
-				echo -e "\nPassword: $password \n"
+                password_length=$1; shift
+                secondary=$1; shift
+                count=$1
 
-		fi
-		;;
+                case $secondary in
+                        -c )
+                                if [ -z "$count" ]
+                                        then
+                                                count=10
+                                fi
+
+                                i=0
+                                if [ -z "$password_length" ]
+                                        then
+                                                while [ $i -le $(( $count - 1)) ]
+                                                do
+                                                        echo $((genrandom_alphanumeric_special 20) | sed "s/./&$(random_special_char)/$(genrandom_number 20)" | sed "s/./&$(random_special_char)/$(genrandom_number 20)")
+                                                        i=$(( $i+1 ))
+                                                done
+                                        else
+                                                while [ $i -le $(( $count - 1)) ]
+                                                do
+                                                        echo $((genrandom_alphanumeric_special $password_length) | sed "s/./&$(random_special_char)/$(genrandom_number $password_length)" | sed "s/./&$(random_special_char)/$(genrandom_number $password_length)")
+                                                        i=$(( $i+1 ))
+                                                done
+                                fi
+                                ;;
+                        * )
+                                if [ -z "$password_length" ]
+                                        then
+						password=$((genrandom_alphanumeric_special 20) | sed "s/./&$(random_special_char)/$(genrandom_number 20)" | sed "s/./&$(random_special_char)/$(genrandom_number 20)")
+                                                echo -e "\nPassword: $password \n"
+                                        else
+						password=$((genrandom_alphanumeric_special $password_length) | sed "s/./&$(random_special_char)/$(genrandom_number $password_length)" | sed "s/./&$(random_special_char)/$(genrandom_number $password_length)")
+                                                echo -e "\nPassword: $password \n"
+                                fi
+                                ;;
+                esac
+                ;;
 
 	#Generate "mixed case word w/ special chars" + number + "mixed case word w/ special chars"
 	#Example: araeotIc(l5folioliferou^s
 	-w )
-		count=$1
+		if [ -e $dictionary ]
+			then
+				count=$1
+			else
+				count=1
+		fi
+
 		i=0
 		if [ -z "$count" ]
 			then
-				echo -e "\nPassword: $(choose_word) \n"
+				if [ -e $dictionary ]
+					then
+						echo -e "\nPassword: $(choose_word) \n"
+					else
+						echo -e "\nDictionary file does not exist.\nIs it in the current directory?\nIs the name of the dictionary file $dictionary?\n"
+				fi
 			else
-				echo ""
 				while [ $i -le $(( $count - 1)) ]
 				do
 					choose_word
 					i=$(( $i+1 ))
 				done
-				echo ""
 		fi
 		;;
 
